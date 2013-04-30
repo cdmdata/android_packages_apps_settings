30b703ee18aac4d87d98b2fb6f67ffe2b68c1763
raf1hh <rszuminski@kbb.com>
2013-04-30 16:10:41 -0700

    Reducing max adjusable brightness by 30%

--- src/com/android/settings/BrightnessPreference.java
+++ src/com/android/settings/BrightnessPreference.java
@@ -46,7 +46,7 @@ public class BrightnessPreference extends SeekBarPreference implements
     // Backlight range is from 0 - 255. Need to make sure that user
     // doesn't set the backlight to 0 and get stuck
     private static final int MINIMUM_BACKLIGHT = android.os.Power.BRIGHTNESS_DIM + 10;
-    private static final int MAXIMUM_BACKLIGHT = android.os.Power.BRIGHTNESS_ON;
+    private static final int MAXIMUM_BACKLIGHT = android.os.Power.BRIGHTNESS_ON - 50;
 
     public BrightnessPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
